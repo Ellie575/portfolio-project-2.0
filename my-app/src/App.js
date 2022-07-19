1abb00b1965f77051dda9c30a57d674c39cca176
Ellie575 <elliekubo@hotmail.com>
2022-07-19 19:53:35 +1000

    Styled homepage hero

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="Hero">
       <h3>Hi, I'm</h3>
       <h1>Ellie Kubo</h1>
-      <h2>Front End Developer based in Sydney</h2>
+      <h2 className="mb-3">Front End Developer based in Sydney</h2>
+      <a
+        href="https://benevolent-fairy-5d3f44.netlify.app"
+        className="btn btn-branding"
+      >
+        Contact me
+      </a>
     </div>
   );
 }
