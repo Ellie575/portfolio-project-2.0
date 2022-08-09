c9a9c9ea350be782d5ea69850c7730a085eb201b
Ellie575 <elliekubo@hotmail.com>
2022-08-09 17:41:41 +1000

    Added mail to link and tweaked wording

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,11 +10,8 @@ function App() {
       <div className="hero">
         <h3>Hi, I'm</h3>
         <h1>Ellie Kubo</h1>
-        <h2 className="mb-3">Front End Developer based in Sydney</h2>
-        <a
-          href="https://benevolent-fairy-5d3f44.netlify.app"
-          className="btn btn-branding"
-        >
+        <h2 className="mb-3">Front End Developer based in Australia</h2>
+        <a href="mailto:eck930705@gmail.com" className="btn btn-branding">
           Contact me
         </a>
       </div>
